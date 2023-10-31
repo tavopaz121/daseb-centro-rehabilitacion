b8c717d4be40dd08c876d64f23ca421e6f79db86
tavopaz121 <tavo.paz12@hotmail.com>
2023-10-31 17:50:53 -0600

    refactor: new changes and styles responsive

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,7 @@
 import type { MetaFunction } from "@remix-run/node";
 
 import logo from "~/assets/imgs/logo.png";
-import banner from "~/assets/imgs/banner-rehabilitacion.webp";
+import banner from "~/assets/imgs/banner.jpg";
 import { useState } from "react";
 
 export const meta: MetaFunction = () => {
@@ -38,7 +38,7 @@ export default function Index() {
   const [showMenu, setShowMenu] = useState(false);
   return (
     <>
-      <header className="shadow-md py-2 px-4 sm:px-10 bg-white font-[sans-serif] min-h-[70px]">
+      <header className="shadow-md py-1 px-4 sm:px-10 bg-white font-[sans-serif] min-h-[70px]">
         <div className="flex flex-wrap items-center justify-between gap-5 relative">
           <a href="">
             <img src={logo} alt="logo" className="w-[180px] max-md:w-[120px]" />
@@ -76,18 +76,68 @@ export default function Index() {
         </div>
       </header>
       <main>
-        <section className="banner">
-          <img src={banner} alt="" className="w-full h-[550px] object-cover" />
-          <div className="hero__banner">
-            <p>
-              ¡BIENVENIDOS! EN NUESTRA CLÍNICA TE BRINDAMOS ATENCIÓN MÉDICA DE
-              CALIDAD, EN EL MOMENTO QUE MÁS LO NECESITES
-            </p>
-            <button className="relative inline-flex items-center justify-center mt-5 mr-2 overflow-hidden text-xl text-white font-semibold rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200">
-              <span className="relative px-8 py-2.5 transition-all ease-in duration-75 bg-transparent rounded-md group-hover:bg-opacity-0">
-                Agendar cita por whatsapp
-              </span>
-            </button>
+        <section className="relative py-10 overflow-hidden bg-black sm:py-16 lg:py-24 xl:py-32">
+          <div className="absolute inset-0">
+            <img
+              className="w-full object-contain max-md:object-cover h-full md:object-left md:scale-150 md:origin-top-left"
+              src={banner}
+              alt=""
+            />
+          </div>
+
+          <div className="absolute inset-0 hidden bg-gradient-to-r md:block from-black to-transparent"></div>
+
+          <div className="absolute inset-0 block bg-black/60 md:hidden"></div>
+
+          <div className="relative px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+            <div className="text-center md:w-2/3 lg:w-1/2 xl:w-1/3 md:text-left mt-8">
+              <h2 className="text-3xl font-bold uppercase leading-tight text-white sm:text-4xl lg:text-5xl">
+                Centro de Rehabilitación Daseb
+              </h2>
+              <p className="mt-4 text-base text-gray-200">
+                ¡Bienvenidos! En nuestra clínica te brindamos atención médica de
+                calidad, en el momento que más lo necesites.
+              </p>
+
+              <form action="#" method="POST" className="mt-8 lg:mt-12">
+                <div className="flex flex-col items-center sm:flex-row sm:justify-center">
+                  <div className="flex-1 w-full min-w-0 px-4 sm:px-0">
+                    <div className="relative text-gray-400 focus-within:text-gray-600">
+                      <label htmlFor="email" className="sr-only"></label>
+                      <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <svg
+                          className="w-5 h-5"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor">
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="1.5"
+                            d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+                          />
+                        </svg>
+                      </div>
+                      <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        placeholder="Enter email address"
+                        className="block w-full py-4 pl-10 pr-4 text-base text-black placeholder-gray-500 transition-all duration-200 border-gray-200 rounded-md sm:rounded-r-none caret-blue-600 focus:border-blue-600 focus:ring-1 focus:ring-blue-600"
+                        required
+                      />
+                    </div>
+                  </div>
+
+                  <button
+                    type="submit"
+                    className="inline-flex items-center justify-center flex-shrink-0 w-auto px-4 py-4 mt-4 font-semibold text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-md sm:mt-0 sm:rounded-l-none sm:w-auto hover:bg-blue-700 focus:bg-blue-700">
+                    Get instant access
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
         </section>
       </main>
