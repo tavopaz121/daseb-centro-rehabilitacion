2a2176c5aa02606544d20abc2cb02ea2ce80c2bb
tavopaz121 <tavo.paz12@hotmail.com>
2023-10-31 12:18:44 -0600

    refactor: new changes and styles responsive

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,6 +2,7 @@ import type { MetaFunction } from "@remix-run/node";
 
 import logo from "~/assets/imgs/logo.png";
 import banner from "~/assets/imgs/banner-rehabilitacion.webp";
+import { useState } from "react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -14,38 +15,65 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+const navLinks = [
+  {
+    name: "inicio",
+    target: "#home",
+  },
+  {
+    name: "nosotros",
+    target: "#home",
+  },
+  {
+    name: "especialidades",
+    target: "#home",
+  },
+  {
+    name: "contacto",
+    target: "#home",
+  },
+];
+
 export default function Index() {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <>
-      <header className="flex w-full justify-between items-center bg-slate-50">
-        <figure>
+      <header className="shadow-md py-2 px-4 sm:px-10 bg-white font-[sans-serif] min-h-[70px]">
+        <div className="flex flex-wrap items-center justify-between gap-5 relative">
           <a href="">
-            <img
-              src={logo}
-              alt="DASEB - Centro de Rehabilitación"
-              className="logo"
-            />
+            <img src={logo} alt="logo" className="w-[180px]" />
           </a>
-        </figure>
-        <nav className="nav">
-          <ul className="flex w-full gap-8 px-10 font-bold">
-            <li>
-              <a href="">Inicio</a>
-            </li>
-            <li>
-              <a href="">Nosotros</a>
-            </li>
-            <li>
-              <a href="">Servicios</a>
-            </li>
-            <li>
-              <a href="">Especialidades</a>
-            </li>
-            <li>
-              <a href="">Contacto</a>
-            </li>
+          <button
+            className="lg:hidden ml-7"
+            onClick={() => setShowMenu(!showMenu)}>
+            <svg
+              className="w-7 h-7"
+              fill="#000"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                fillRule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clipRule="evenodd"></path>
+            </svg>
+          </button>
+          <ul
+            className={`flex lg:space-x-5 max-lg:space-y-2 max-lg:block max-lg:w-full ${
+              showMenu && "show__menu"
+            }`}>
+            {navLinks.map((link) => (
+              <li
+                className="max-lg:border-b max-lg:py-2 px-3 max-lg:rounded"
+                key={link.name}>
+                <a
+                  href={link.target}
+                  className="lg:hover:text-[#007bff] text-gray-500 block font-semibold text-[15px] capitalize">
+                  {link.name}
+                </a>
+              </li>
+            ))}
           </ul>
-        </nav>
+        </div>
       </header>
       <main>
         <section className="banner">
