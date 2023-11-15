b462eacde6b0d3cd6ad2458b59dc6047aaad4546
tavopaz121 <tavo.paz12@hotmail.com>
2023-11-15 16:32:34 -0600

    refactor: add icons

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,11 +5,11 @@ import banner from "~/assets/imgs/banner.jpg";
 import { useState } from "react";
 
 import servicio1 from "~/assets/imgs/servicio-de-ciguria.png";
-import servicio2 from "~/assets/imgs/clinica-heridas-estomas-01-1.png";
-import servicio3 from "~/assets/imgs/servicio-de-hospitalizacion.png";
-import servicio4 from "~/assets/imgs/servicio-de-laboratorio-de-analisis-clinicos.png";
-import servicio5 from "~/assets/imgs/servicio-de-terapia-interna.png";
-import servicio6 from "~/assets/imgs/servicio-de-clinica-de-diabetes.png";
+// import servicio2 from "~/assets/imgs/clinica-heridas-estomas-01-1.png";
+// import servicio3 from "~/assets/imgs/servicio-de-hospitalizacion.png";
+// import servicio4 from "~/assets/imgs/servicio-de-laboratorio-de-analisis-clinicos.png";
+// import servicio5 from "~/assets/imgs/servicio-de-terapia-interna.png";
+// import servicio6 from "~/assets/imgs/servicio-de-clinica-de-diabetes.png";
 
 export const meta: MetaFunction = () => {
   return [
@@ -132,30 +132,12 @@ export default function Index() {
             <div className="grid grid-cols-1 gap-12 text-center sm:grid-cols-2 md:grid-cols-3 lg:gap-y-16">
               <div>
                 <div className="relative flex items-center justify-center mx-auto">
-                  <svg
-                    className="text-blue-100"
-                    width="72"
-                    height="75"
-                    viewBox="0 0 72 75"
-                    fill="currentColor"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path d="M63.6911 28.8569C68.0911 48.8121 74.6037 61.2674 53.2349 65.9792C31.8661 70.6909 11.6224 61.2632 7.22232 41.308C2.82229 21.3528 3.6607 12.3967 25.0295 7.68503C46.3982 2.97331 59.2911 8.90171 63.6911 28.8569Z" />
-                  </svg>
-                  <svg
-                    className="absolute text-blue-600 w-9 h-9"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="1.5"
-                      d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4"
-                    />
-                  </svg>
+                  <img
+                    src={servicio1}
+                    className="w-[150px] h-[150px]"
+                    alt="Cirujia"></img>
                 </div>
-                <h3 className="mt-8 text-lg font-semibold text-black">
+                <h3 className="mt-2 text-lg font-semibold text-black">
                   Secured Payments
                 </h3>
                 <p className="mt-4 text-base text-gray-600">
@@ -302,31 +284,10 @@ export default function Index() {
 
               <div>
                 <div className="relative flex items-center justify-center mx-auto">
-                  <svg
-                    className="text-yellow-100"
-                    width="78"
-                    height="78"
-                    viewBox="0 0 78 78"
-                    fill="currentColor"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path d="M8.49996 28.0002C4.09993 47.9554 14.1313 66.7885 35.5 71.5002C56.8688 76.2119 68.0999 58.4553 72.5 38.5001C76.9 18.5449 68.3688 12.711 47 7.99931C25.6312 3.28759 12.9 8.04499 8.49996 28.0002Z" />
-                  </svg>
-                  <svg
-                    className="absolute text-yellow-500 w-9 h-9"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="1.5"
-                      d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4"
-                    />
-                  </svg>
+                  <p>Fast</p>
                 </div>
                 <h3 className="mt-8 text-lg font-semibold text-black">
-                  Secured Payments
+                  Secured Ppapa
                 </h3>
                 <p className="mt-4 text-base text-gray-600">
                   Amet minim mollit non deserunt ullamco est sit aliqua dolor do
