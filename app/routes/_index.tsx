971f906667cf56ff904f89409dce7aa366725c29
tavopaz121 <tavo.paz12@hotmail.com>
2023-11-15 16:45:55 -0600

    refactor: changes incons

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,11 +5,11 @@ import banner from "~/assets/imgs/banner.jpg";
 import { useState } from "react";
 
 import servicio1 from "~/assets/imgs/servicio-de-ciguria.png";
-// import servicio2 from "~/assets/imgs/clinica-heridas-estomas-01-1.png";
-// import servicio3 from "~/assets/imgs/servicio-de-hospitalizacion.png";
-// import servicio4 from "~/assets/imgs/servicio-de-laboratorio-de-analisis-clinicos.png";
-// import servicio5 from "~/assets/imgs/servicio-de-terapia-interna.png";
-// import servicio6 from "~/assets/imgs/servicio-de-clinica-de-diabetes.png";
+import servicio2 from "~/assets/imgs/clinica-heridas-estomas-01-1.png";
+import servicio3 from "~/assets/imgs/servicio-de-hospitalizacion.png";
+import servicio4 from "~/assets/imgs/servicio-de-laboratorio-de-analisis-clinicos.png";
+import servicio5 from "~/assets/imgs/servicio-de-terapia-interna.png";
+import servicio6 from "~/assets/imgs/servicio-de-clinica-de-diabetes.png";
 
 export const meta: MetaFunction = () => {
   return [
@@ -47,7 +47,7 @@ export default function Index() {
     <>
       <header className="shadow-md py-1 px-4 sm:px-10 bg-white font-[sans-serif] min-h-[70px]">
         <div className="flex flex-wrap items-center justify-between gap-5 relative">
-          <a href="">
+          <a href="/">
             <img src={logo} alt="logo" className="w-[180px] max-md:w-[120px]" />
           </a>
           <button
@@ -137,9 +137,7 @@ export default function Index() {
                     className="w-[150px] h-[150px]"
                     alt="Cirujia"></img>
                 </div>
-                <h3 className="mt-2 text-lg font-semibold text-black">
-                  Secured Payments
-                </h3>
+
                 <p className="mt-4 text-base text-gray-600">
                   Amet minim mollit non deserunt ullamco est sit aliqua dolor do
                   amet sint. Velit officia consequat duis enim velit mollit.
@@ -148,32 +146,11 @@ export default function Index() {
 
               <div>
                 <div className="relative flex items-center justify-center mx-auto">
-                  <svg
-                    className="text-orange-100"
-                    width="62"
-                    height="64"
-                    viewBox="0 0 62 64"
-                    fill="currentColor"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path d="M62 13.001C62 33.4355 53.9345 64.001 33.5 64.001C13.0655 64.001 0 50.435 0 30.0005C0 9.56596 2.56546 4.00021 23 4.00021C43.4345 4.00021 62 -7.43358 62 13.001Z" />
-                  </svg>
-                  <svg
-                    className="absolute text-orange-600 w-9 h-9"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="1.5"
-                      d="M13 10V3L4 14h7v7l9-11h-7z"
-                    />
-                  </svg>
+                  <img
+                    src={servicio2}
+                    className="w-[150px] h-[150px]"
+                    alt="Cirujia"></img>
                 </div>
-                <h3 className="mt-8 text-lg font-semibold text-black">
-                  Fast & Easy to Load
-                </h3>
                 <p className="mt-4 text-base text-gray-600">
                   Amet minim mollit non deserunt ullamco est sit aliqua dolor do
                   amet sint. Velit officia consequat duis enim velit mollit.
@@ -182,32 +159,12 @@ export default function Index() {
 
               <div>
                 <div className="relative flex items-center justify-center mx-auto">
-                  <svg
-                    className="text-green-100"
-                    width="66"
-                    height="68"
-                    viewBox="0 0 66 68"
-                    fill="currentColor"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path d="M65.5 30C65.5 50.4345 46.4345 68 26 68C5.56546 68 0 50.4345 0 30C0 9.56546 12.5655 0 33 0C53.4345 0 65.5 9.56546 65.5 30Z" />
-                  </svg>
-                  <svg
-                    className="absolute text-green-600 w-9 h-9"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="1.5"
-                      d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                    />
-                  </svg>
+                  <img
+                    src={servicio3}
+                    className="w-[150px] h-[150px]"
+                    alt="Cirujia"></img>
                 </div>
-                <h3 className="mt-8 text-lg font-semibold text-black">
-                  Light & Dark Version
-                </h3>
+
                 <p className="mt-4 text-base text-gray-600">
                   Amet minim mollit non deserunt ullamco est sit aliqua dolor do
                   amet sint. Velit officia consequat duis enim velit mollit.
@@ -216,32 +173,12 @@ export default function Index() {
 
               <div>
                 <div className="relative flex items-center justify-center mx-auto">
-                  <svg
-                    className="text-purple-100"
-                    width="66"
-                    height="68"
-                    viewBox="0 0 66 68"
-                    fill="currentColor"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path d="M65.5 30C65.5 50.4345 46.4345 68 26 68C5.56546 68 0 50.4345 0 30C0 9.56546 12.5655 0 33 0C53.4345 0 65.5 9.56546 65.5 30Z" />
-                  </svg>
-                  <svg
-                    className="absolute text-purple-600 w-9 h-9"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="1.5"
-                      d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                    />
-                  </svg>
+                  <img
+                    src={servicio4}
+                    className="w-[150px] h-[150px]"
+                    alt="Cirujia"></img>
                 </div>
-                <h3 className="mt-8 text-lg font-semibold text-black">
-                  Light & Dark Version
-                </h3>
+
                 <p className="mt-4 text-base text-gray-600">
                   Amet minim mollit non deserunt ullamco est sit aliqua dolor do
                   amet sint. Velit officia consequat duis enim velit mollit.
@@ -250,32 +187,12 @@ export default function Index() {
 
               <div>
                 <div className="relative flex items-center justify-center mx-auto">
-                  <svg
-                    className="text-gray-100"
-                    width="65"
-                    height="70"
-                    viewBox="0 0 65 70"
-                    fill="currentColor"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path d="M64.5 26C64.5 46.4345 56.4345 70 36 70C15.5655 70 0 53.9345 0 33.5C0 13.0655 13.0655 0 33.5 0C53.9345 0 64.5 5.56546 64.5 26Z" />
-                  </svg>
-                  <svg
-                    className="absolute text-gray-600 w-9 h-9"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="1.5"
-                      d="M13 10V3L4 14h7v7l9-11h-7z"
-                    />
-                  </svg>
+                  <img
+                    src={servicio5}
+                    className="w-[150px] h-[150px]"
+                    alt="Cirujia"></img>
                 </div>
-                <h3 className="mt-8 text-lg font-semibold text-black">
-                  Fast & Easy to Load
-                </h3>
+
                 <p className="mt-4 text-base text-gray-600">
                   Amet minim mollit non deserunt ullamco est sit aliqua dolor do
                   amet sint. Velit officia consequat duis enim velit mollit.
@@ -284,114 +201,12 @@ export default function Index() {
 
               <div>
                 <div className="relative flex items-center justify-center mx-auto">
-                  <p>Fast</p>
+                  <img
+                    src={servicio6}
+                    className="w-[150px] h-[150px]"
+                    alt="Cirujia"></img>
                 </div>
-                <h3 className="mt-8 text-lg font-semibold text-black">
-                  Secured Ppapa
-                </h3>
-                <p className="mt-4 text-base text-gray-600">
-                  Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-                  amet sint. Velit officia consequat duis enim velit mollit.
-                </p>
-              </div>
 
-              <div>
-                <div className="relative flex items-center justify-center mx-auto">
-                  <svg
-                    className="text-gray-100"
-                    width="62"
-                    height="64"
-                    viewBox="0 0 62 64"
-                    fill="currentColor"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path d="M62 13.001C62 33.4355 53.9345 64.001 33.5 64.001C13.0655 64.001 0 50.435 0 30.0005C0 9.56596 2.56546 4.00021 23 4.00021C43.4345 4.00021 62 -7.43358 62 13.001Z"></path>
-                  </svg>
-                  <svg
-                    className="absolute text-gray-600 w-9 h-9"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="1.5"
-                      d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                    />
-                  </svg>
-                </div>
-                <h3 className="mt-8 text-lg font-semibold text-black">
-                  Light & Dark Version
-                </h3>
-                <p className="mt-4 text-base text-gray-600">
-                  Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-                  amet sint. Velit officia consequat duis enim velit mollit.
-                </p>
-              </div>
-
-              <div>
-                <div className="relative flex items-center justify-center mx-auto">
-                  <svg
-                    className="text-rose-100"
-                    width="72"
-                    height="75"
-                    viewBox="0 0 72 75"
-                    fill="currentColor"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path d="M63.6911 28.8569C68.0911 48.8121 74.6037 61.2674 53.2349 65.9792C31.8661 70.6909 11.6224 61.2632 7.22232 41.308C2.82229 21.3528 3.6607 12.3967 25.0295 7.68503C46.3982 2.97331 59.2911 8.90171 63.6911 28.8569Z" />
-                  </svg>
-                  <svg
-                    className="absolute text-rose-600 w-9 h-9"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="1.5"
-                      d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4"
-                    />
-                  </svg>
-                </div>
-                <h3 className="mt-8 text-lg font-semibold text-black">
-                  Secured Payments
-                </h3>
-                <p className="mt-4 text-base text-gray-600">
-                  Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-                  amet sint. Velit officia consequat duis enim velit mollit.
-                </p>
-              </div>
-
-              <div>
-                <div className="relative flex items-center justify-center mx-auto">
-                  <svg
-                    className="text-lime-100"
-                    width="62"
-                    height="65"
-                    viewBox="0 0 62 65"
-                    fill="currentColor"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path d="M0 13.0264C0 33.4609 8.06546 64.0264 28.5 64.0264C48.9345 64.0264 62 50.4604 62 30.0259C62 9.59135 59.4345 4.0256 39 4.0256C18.5655 4.0256 0 -7.40819 0 13.0264Z" />
-                  </svg>
-
-                  <svg
-                    className="absolute text-lime-600 w-9 h-9"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="1.5"
-                      d="M13 10V3L4 14h7v7l9-11h-7z"
-                    />
-                  </svg>
-                </div>
-                <h3 className="mt-8 text-lg font-semibold text-black">
-                  Fast & Easy to Load
-                </h3>
                 <p className="mt-4 text-base text-gray-600">
                   Amet minim mollit non deserunt ullamco est sit aliqua dolor do
                   amet sint. Velit officia consequat duis enim velit mollit.
